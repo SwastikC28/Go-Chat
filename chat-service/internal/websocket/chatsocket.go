1ef33cf3f0735b97d84393783e92b777e16bdcdc
Swastik Chaudhary <swastikkumar50@gmail.com>
2025-01-02 20:35:28 +0530

    Added pool and mutex in chat socket handler

--- chat-service/internal/websocket/chatsocket.go
+++ chat-service/internal/websocket/chatsocket.go
@@ -2,6 +2,7 @@ package websocket
 
 import (
 	"net/http"
+	"sync"
 
 	"shared/socket"
 
@@ -10,21 +11,31 @@ import (
 )
 
 type ChatWebSocket struct {
+	pool  *socket.Pool
+	mutex *sync.Mutex
 }
 
-func NewChatWebSocket() socket.WebSocket {
-	return &ChatWebSocket{}
+func NewChatWebSocket(pool *socket.Pool, mutex *sync.Mutex) socket.WebSocket {
+	return &ChatWebSocket{
+		pool:  pool,
+		mutex: mutex,
+	}
 }
 
 func (chatsocket *ChatWebSocket) RegisterSocket(router *mux.Router) {
+	router.HandleFunc("/ws", chatsocket.Upgrade)
 }
 
 func (chatsocket *ChatWebSocket) Upgrade(w http.ResponseWriter, r *http.Request) {
 	logger := zerolog.Ctx(r.Context()).With().Str("action", "chatwebsocket:upgrade").Logger()
-	_, err := socket.CreateWebSocket(w, r)
+	conn, err := socket.CreateWebSocket(w, r)
 	if err != nil {
 		logger.Err(err).Msg("There was some error upgrading connection")
 		return
 	}
 
+	client := socket.NewClient(conn, chatsocket.pool, chatsocket.mutex)
+	chatsocket.pool.Register <- client
+
+	client.Read()
 }
